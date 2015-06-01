439b162303401a86b56348d09015e8f894227885
Dor-Ron <dor.rondel@gmail.com>
2015-06-01 01:09:45 -0400

    added lucky_sum

---
+++ Logic-2/lucky_sum.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python
+
+'''
+Instructions:
+Given 3 int values, a b c, return their sum.
+ However, if one of the values is 13 then it does not count towards the sum
+ and values to its right do not count. So for example, if b is 13, then both b and c do not count. 
+'''
+
+def lucky_sum(a, b, c):
+  if a == 13 and b == 13:
+    return 0
+  if a == 13:
+    return 0
+  if b == 13:
+    return a
+  if c == 13:
+    return a + b
+  else:
+    return a + b + c
