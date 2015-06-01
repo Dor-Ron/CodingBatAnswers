14d53ce27ff8b2cce383e40e59002de2ca8dfaff
Dor-Ron <dor.rondel@gmail.com>
2015-06-01 01:01:23 -0400

    Update make_bricks.py

--- Logic-2/make_bricks.py
+++ Logic-2/make_bricks.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python
 
 '''
+Instructions:
 We want to make a row of bricks that is goal inches long. We have a number of small bricks
 (1 inch each) and big bricks (5 inches each).
  Return True if it is possible to make the goal by choosing from the given bricks.
