106a2d42d45906f206419511784470e8082029e2
Dor-Ron <dor.rondel@gmail.com>
2015-06-14 15:35:01 -0400

    Update sum67.py
    Added instructions for sum67.py FINISHED CODING BAT'S PYTHON TRACK!!

--- List-2/sum67.py
+++ List-2/sum67.py
@@ -1,5 +1,12 @@
 #!/usr/bin/env python
 
+'''
+Instructions:
+Return the sum of the numbers in the array, except ignore sections of numbers starting with a 6
+and extending to the next 7 (every 6 will be followed by at least one 7).
+Return 0 for no numbers. 
+'''
+
 def sum67(nums):
   for k in xrange(len(nums)):
     if nums[k] == 6:
