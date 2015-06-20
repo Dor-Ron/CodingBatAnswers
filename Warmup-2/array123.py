53c4db6ea6f05577c74ad8e1f4331130da9b91b0
Dor-Ron <dor.rondel@gmail.com>
2015-06-20 01:51:56 -0400

    Update array123.py
    added array123 one-liner

--- Warmup-2/array123.py
+++ Warmup-2/array123.py
@@ -1,10 +1,14 @@
 #Instructions
 #Given an array of ints, return True if .. 1, 2, 3, .. appears in the array somewhere. 
 
-
+#original solution
 def array123(nums):
   a,b,c = 1,2,3
   if a and b and c in nums:
     return True
   else:
     return False
+    
+#one-liner solution
+def array123(nums):
+  return 1 and 2 and 3 in nums
