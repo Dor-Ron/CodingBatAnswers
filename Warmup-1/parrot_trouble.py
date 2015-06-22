ea4aeb0f99c00a7af5d0072d9065ef4ad650331d
Dor-Ron <dor.rondel@gmail.com>
2015-06-22 12:02:59 -0400

    Update parrot_trouble.py

--- Warmup-1/parrot_trouble.py
+++ Warmup-1/parrot_trouble.py
@@ -2,9 +2,13 @@
 #We have a loud talking parrot. The "hour" parameter is the current hour time in the range 0..23. 
 #We are in trouble if the parrot is talking and the hour is before 7 or after 20. Return True if we are in trouble. 
 
-
+#original solution
 def parrot_trouble(talking, hour):
   if talking == True and (hour < 7 or hour > 20):
     return True
   else:
     return False
+
+#one liner
+def parrot_trouble(talking, hour):
+  return talking and (hour < 7 or hour > 20)
