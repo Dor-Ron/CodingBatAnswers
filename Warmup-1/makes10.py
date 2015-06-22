87d9c641dbba797c0ecbda2e0aa2425f0a4e1f7a
Dor-Ron <dor.rondel@gmail.com>
2015-06-22 11:59:26 -0400

    Update makes10.py

--- Warmup-1/makes10.py
+++ Warmup-1/makes10.py
@@ -1,10 +1,13 @@
 #Instructions
 #Given 2 ints, a and b, return True if one if them is 10 or if their sum is 10. 
 
-
-
+#original solution
 def makes10(a, b):
   if (a == 10 or b == 10) or (a+b == 10):
     return True
   else:
     return False
+
+#one-liner
+def makes10(a, b):
+  return (a == 10 or b == 10) or (a+b == 10)
