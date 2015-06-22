7223d3a7b65d75fe6fbc9e3c9ce33fdc1dc79b57
Dor-Ron <dor.rondel@gmail.com>
2015-06-22 12:08:07 -0400

    Update sum_double.py

--- Warmup-1/sum_double.py
+++ Warmup-1/sum_double.py
@@ -2,8 +2,13 @@
 #Given two int values, return their sum. Unless the two values are the same, then return double their sum. 
 
 
+#original solution
 def sum_double(a, b):
   if a != b:
     return a+b
   else:
     return (a+b)*2
+
+#one-liner
+def sum_double(a, b):
+  return a + b if a != b else (a + b) * 2
