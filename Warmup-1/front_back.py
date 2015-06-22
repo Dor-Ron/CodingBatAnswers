7b76fd361d60314638294c1271ae2930b300e50b
Dor-Ron <dor.rondel@gmail.com>
2015-06-22 11:58:35 -0400

    Update front_back.py

--- Warmup-1/front_back.py
+++ Warmup-1/front_back.py
@@ -1,11 +1,18 @@
 #Instructions
 #Given a string, return a new string where the first and last chars have been exchanged. 
 
+#original solution
 def front_back(str):
   first = str[0]
   root = str[1:-1]
   last = str[-1]
   return last + root + first
   
+#one-liner
+def front_back(str):
+  return str[-1] + str[1:-1] + str[0]
+
 #The code didn't work on the online compiler, it got an index error.
 #However, it ran perfectly fine and as planned locally.
+
+
