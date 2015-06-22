cb76b5e64ab13750c167a9c585e016f0d58570b5
Dor-Ron <dor.rondel@gmail.com>
2015-06-22 16:56:39 -0400

    Update last2.py

--- Warmup-2/last2.py
+++ Warmup-2/last2.py
@@ -2,16 +2,12 @@
 #Given a string, return the count of the number of times that a substring length 2 appears in the string
 #and also as the last 2 chars of the string, so "hixxxhi" yields 1 (we won't count the end substring). 
 
-
+#original solution
 def last2(str):
-  seq = str[:2]
   count = 0
-  for i in str[-2]:
-    if i + str[(str.index(i) + 1)] == seq:
+  for two in range(len(str) - 2):
+    if str[two: two + 2] == str[-2:]:
       count += 1
-    else:
-      continue
   return count
 
-#Once again, the solution I came up with didn't work online, another index error.
-#However, it ran fine locally so I'm complacent with it.
+
