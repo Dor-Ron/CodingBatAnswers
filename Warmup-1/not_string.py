08a354cc01f6bec00344c85617f8c812d46bdcbe
Dor-Ron <dor.rondel@gmail.com>
2015-06-22 12:02:01 -0400

    Update not_string.py

--- Warmup-1/not_string.py
+++ Warmup-1/not_string.py
@@ -2,9 +2,13 @@
 #Given a string, return a new string where "not " has been added to the front. 
 #However, if the string already begins with "not", return the string unchanged. 
 
-
+#original solution
 def not_string(str):
   if str[:3] == "not":
     return str
   else:
     return "not " + str
+
+#one-liner
+def not_string(str):
+  return str if str[:3] == "not" else "not " + str
