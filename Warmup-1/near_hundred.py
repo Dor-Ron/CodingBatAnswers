0915dd591e9495924126a783c88f1de2da2e8782
Dor-Ron <dor.rondel@gmail.com>
2015-06-22 12:00:56 -0400

    Update near_hundred.py

--- Warmup-1/near_hundred.py
+++ Warmup-1/near_hundred.py
@@ -1,9 +1,13 @@
 #Instructions
 #Given an int n, return True if it is within 10 of 100 or 200.
 
-
+#original solution
 def near_hundred(n):
   if abs(n-100) <= 10 or abs(n-200) <= 10:
     return True
   else:
     return False
+
+#one-liner
+def near_hundred(n):
+  return abs(n-100) <= 10 or abs(n-200) <= 10
