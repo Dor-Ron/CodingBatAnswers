a948410825b42e3918175707dac0fafc259ecd6f
Dor-Ron <dor.rondel@gmail.com>
2015-06-22 23:10:53 -0400

    added combo_string and firsrt_two one-liners. Finished one-liners for String-1

--- String-1/combo_string.py
+++ String-1/combo_string.py
@@ -3,9 +3,13 @@
 #with the shorter string on the outside and the longer string on the inside. 
 #The strings will not be the same length, but they may be empty (length 0). 
 
-
+#original solution
 def combo_string(a, b): 
   if len(a) > len(b):
     return b + a + b
   else:
     return a + b + a
+
+#one-liner
+def combo_string(a, b): 
+  return b + a + b if len(a) > len(b) else a + b + a
--- String-1/first_two.py
+++ String-1/first_two.py
@@ -3,8 +3,13 @@
 #so the String "Hello" yields "He". If the string is shorter than length 2, 
 #return whatever there is, so "X" yields "X", and the empty string "" yields the empty string "". 
 
+#original solution
 def first_two(str):
   if len(str) < 2:
     return str
   else:
     return str[:2]
+
+#one liner
+def first_two(str):
+  return str if len(str) < 2 else str[:2]
